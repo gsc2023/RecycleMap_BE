4393ea7df1cd11d18319ef54ba00e18d113237b5
zcae1020 <zcae1020@naver.com>
2023-02-21 16:14:11 +0900

    fix: report api error 처리
    error에 맞게 http status 처리

--- repository/report.go
+++ repository/report.go
@@ -10,31 +10,36 @@ import (
 	"google.golang.org/api/iterator"
 )
 
-func SaveReport(report domain.Report) (*firestore.DocumentRef, *firestore.WriteResult) {
+func SaveReport(UID string, report domain.ReportDao) (*firestore.DocumentRef, *firestore.WriteResult, error) {
+	report.UID = UID
 	ref, wr, err := config.GetFirestore().Collection("reports").Add(config.Ctx, report)
 	if err != nil {
 		log.Printf("error save report: %v\n", err)
 	}
 
-	return ref, wr
+	return ref, wr, err
 }
 
-func FindReportByID(ID string) domain.ReportDto {
+func FindReportByID(ID string) (domain.ReportDto, error) {
+	report := domain.ReportDao{}
+
 	dsnap, err := config.GetFirestore().Collection("reports").Doc(ID).Get(config.Ctx)
 	if err != nil {
 		log.Printf("error find report by id: %v\n", err)
+		return domain.ReportDto{ID: ID, Report: report}, err
 	}
 
-	report := domain.Report{}
 	err = mapstructure.Decode(dsnap.Data(), &report)
 	if err != nil {
 		log.Printf("error find report by id: %v\n", err)
+		return domain.ReportDto{ID: ID, Report: report}, err
 	}
 
-	return domain.ReportDto{ID: ID, Report: report}
+	return domain.ReportDto{ID: ID, Report: report}, err
 }
 
-func FindAllReports() (reportDtos []domain.ReportDto) {
+func FindAllReports() ([]domain.ReportDto, error) {
+	reportDtos := []domain.ReportDto{}
 	iter := config.GetFirestore().Collection("reports").Documents(config.Ctx)
 	for {
 		doc, err := iter.Next()
@@ -43,55 +48,61 @@ func FindAllReports() (reportDtos []domain.ReportDto) {
 		}
 		if err != nil {
 			log.Printf("error find all reports: %v\n", err)
+			return reportDtos, err
 		}
 
-		report := domain.Report{}
+		report := domain.ReportDao{}
 		err = mapstructure.Decode(doc.Data(), &report)
 		if err != nil {
 			log.Printf("error find all reports: %v\n", err)
+			return reportDtos, err
 		}
 
 		reportDtos = append(reportDtos, domain.ReportDto{ID: doc.Ref.ID, Report: report})
 	}
 
-	return
+	return reportDtos, nil
 }
 
-func SetReport(ID string, report domain.Report) *firestore.WriteResult {
+func SetReport(ID string, report domain.ReportDao) (*firestore.WriteResult, error) {
 	wr, err := config.GetFirestore().Collection("reports").Doc(ID).Set(config.Ctx, report)
 	if err != nil {
 		log.Printf("error set report: %v\n", err)
 	}
 
-	return wr
+	return wr, err
 }
 
-func FindReportsByUserId(userID string) (reportDtos []domain.ReportDto) {
-	iter := config.GetFirestore().Collection("reports").Where("userID", "==", userID).Documents(config.Ctx)
+func FindReportsByUId(UID string) ([]domain.ReportDto, error) {
+	reportDtos := []domain.ReportDto{}
+	iter := config.GetFirestore().Collection("reports").Where("UID", "==", UID).Documents(config.Ctx)
 	for {
 		doc, err := iter.Next()
 		if err == iterator.Done {
 			break
 		}
 		if err != nil {
-			log.Printf("error find reports by userID: %v\n", err)
+			log.Printf("error find reports by UID: %v\n", err)
+			return reportDtos, err
 		}
 
-		report := domain.Report{}
+		report := domain.ReportDao{}
 		err = mapstructure.Decode(doc.Data(), &report)
 		if err != nil {
-			log.Printf("error find reports by userID: %v\n", err)
+			log.Printf("error find reports by UID: %v\n", err)
+			return reportDtos, err
 		}
 
 		reportDtos = append(reportDtos, domain.ReportDto{ID: doc.Ref.ID, Report: report})
 	}
 
-	return
+	return reportDtos, nil
 }
 
-func DelReport(ID string) {
+func DelReport(ID string) error {
 	_, err := config.GetFirestore().Collection("reports").Doc(ID).Delete(config.Ctx)
 	if err != nil {
 		log.Printf("error delete report: %v\n", err)
 	}
+	return err
 }
