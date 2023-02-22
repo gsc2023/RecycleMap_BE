52270615d326a85e44e2284dc7d8e9e69600112c
suin park <tndls3026@ajou.ac.kr>
2023-02-22 19:21:59 +0900

    feat:특정 location에 comment 작성 기능

--- repository/comment.go
+++ repository/comment.go
@@ -1,17 +1,21 @@
 package repository
 
 import (
+	"domain"
 	"log"
 	"module/config"
 
 	"cloud.google.com/go/firestore"
 )
 
-func SaveComment(ID string, content string, uID string) (*firestore.DocumentRef, *firestore.WriteResult) {
+func SaveComment(UID string, ID string, comment domain.Comment) (*firestore.DocumentRef, *firestore.WriteResult, error) {
+	comment.UID = UID
+	comment.LocationID = ID
+
 	ref, wr, err := config.GetFirestore().Collection("comments").Add(config.Ctx, comment)
 	if err != nil {
-		log.Printf("error save comment: %v\n", err)
+		log.Printf("error SaveComment: %v\n", err)
 	}
 
-	return ref, wr
+	return ref, wr, err
 }
--- service/comment.go
+++ service/comment.go
@@ -1,11 +1,13 @@
 package service
 
 import (
+	"domain"
 	"repository"
 
 	"cloud.google.com/go/firestore"
+	"firebase.google.com/go/auth"
 )
 
-func SaveComment(ID string, content string, uID string) (*firestore.DocumentRef, *firestore.WriteResult) {
-	return repository.SaveComment(ID, content, uID)
+func JoinComment(token *auth.Token, ID string, comment domain.Comment) (*firestore.DocumentRef, *firestore.WriteResult, error) {
+	return repository.SaveComment(token.UID, ID, comment)
 }
